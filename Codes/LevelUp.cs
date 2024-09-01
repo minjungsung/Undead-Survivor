3573a48f65c7cb0cd1fd68d2087f2062b7a22f7c
Minjung Sung <minjungsung1994@gmail.com>
2024-09-02 00:05:55 +0900

    chore: Add new scripts and data for character, items, and UI elements

---
+++ Codes/LevelUp.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUp : MonoBehaviour
+{
+    RectTransform rect;
+    Item[] items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        items = GetComponentsInChildren<Item>(true);
+    }
+
+    public void Show()
+    {
+        Next();
+        rect.localScale = Vector3.one;
+        GameManager.instance.Stop();
+    }
+
+    public void Hide()
+    {
+        rect.localScale = Vector3.zero;
+        GameManager.instance.Resume();
+    }
+
+    public void Select(int index)
+    {
+        items[index].OnClick();
+    }
+
+    void Next()
+    {
+        // 1. 모든 아이템 비활성화
+        foreach(Item item in items)
+        {
+            item.gameObject.SetActive(false);
+        }
+
+        // 2. 그중 랜덤 3개만 아이템 활성
+        int[] ran = new int[3];
+        while (true)
+        {
+            ran[0] = Random.Range(0, items.Length);
+            ran[1] = Random.Range(0, items.Length);
+            ran[2] = Random.Range(0, items.Length);
+
+            if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
+            {
+                break;
+            }
+        }
+
+        for (int index=0; index < ran.Length; index++)
+        {
+            Item ranItem = items[ran[index]];
+            // 3. 만렙 아이템의 경우는 소비아이템으로 대체
+            if (ranItem.level == ranItem.data.damages.Length)
+            {
+                items[4].gameObject.SetActive(true);
+            } else
+            {
+                ranItem.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Codes/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Scanner scanner;
+    public Hand[] hands;
+    public RuntimeAnimatorController[] animCon;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+        hands = GetComponentsInChildren<Hand>(true);
+    }
+
+    void OnEnable()
+    {
+        speed *= Character.Speed;
+        anim.runtimeAnimatorController = animCon[GameManager.instance.playerId];
+    }
+
+    void Update()
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!GameManager.instance.isLive)
+        {
+            return;
+        }
+        GameManager.instance.health -= 10 * Time.deltaTime;
+        if (GameManager.instance.health < 0)
+        {
+            for (int index = 2; index < transform.childCount; index++)
+            {
+                transform.GetChild(index).gameObject.SetActive(false);
+            }
+            anim.SetTrigger("Dead");
+            GameManager.instance.GameOver();
+        }
+    }
+}
